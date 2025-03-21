b7970996061d293433ed5f4406cb87678c6f1fed
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-03-21 18:34:31 -0400

    change images and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/logo.jpg'
 import './App.css'
 
 function App() {
@@ -9,25 +9,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://peggy1008123.github.io/" target="_blank">
+          <img src={Logo} className="logo" alt="Pei logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Thanks for your guidance!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        Click until you're bored {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
